80e4566f02411a2f8f740fcd7d5c7dfddc307c3f
Keith Tomlinson <keith.tomlinson@reeher.com>
2020-04-26 11:29:47 -0500

    Seciton 10 #119 - 122

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,40 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
-
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter();
 
   private recipes: Recipe[] = [
-    new Recipe('Pizza', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet lectus justo. Ut mollis, purus sit amet rutrum vestibulum, urna ipsum placerat dui, sit amet efficitur sapien ante vel sapien.', 'https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/a019-jakubk-0793-crusty-pizza-with-salami-mushrooms-onion.jpg?w=800&dpr=1&fit=default&crop=default&auto=format&fm=pjpg&q=75&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=5e653010dd06591c50b3bd75aa332dc6'),
-    new Recipe('Coffee', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet lectus justo. Ut mollis, purus sit amet rutrum vestibulum, urna ipsum placerat dui, sit amet efficitur sapien ante vel sapien.','https://c0.wallpaperflare.com/preview/62/451/829/coffee-cold-freddo-cafe.jpg'),
-    new Recipe('Carrots','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet lectus justo. Ut mollis, purus sit amet rutrum vestibulum, urna ipsum placerat dui, sit amet efficitur sapien ante vel sapien.', 'https://upload.wikimedia.org/wikipedia/commons/3/32/Carrots_of_many_colors.jpg')
+    new Recipe(
+      'Pizza',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet lectus justo. Ut mollis, purus sit amet rutrum vestibulum, urna ipsum placerat dui, sit amet efficitur sapien ante vel sapien.',
+      'https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/a019-jakubk-0793-crusty-pizza-with-salami-mushrooms-onion.jpg?w=800&dpr=1&fit=default&crop=default&auto=format&fm=pjpg&q=75&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=5e653010dd06591c50b3bd75aa332dc6',
+      [new Ingredient('mozzarella', 2), new Ingredient('pizza sauce', 1)]),
+    new Recipe(
+      'Coffee',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet lectus justo. Ut mollis, purus sit amet rutrum vestibulum, urna ipsum placerat dui, sit amet efficitur sapien ante vel sapien.',
+      'https://c0.wallpaperflare.com/preview/62/451/829/coffee-cold-freddo-cafe.jpg',
+      [new Ingredient('dark roast coffee', 2), new Ingredient('milk', 1)]),
+    new Recipe(
+      'Carrots',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet lectus justo. Ut mollis, purus sit amet rutrum vestibulum, urna ipsum placerat dui, sit amet efficitur sapien ante vel sapien.',
+      'https://upload.wikimedia.org/wikipedia/commons/3/32/Carrots_of_many_colors.jpg',
+      [new Ingredient('carrots', 2), new Ingredient('dill', 1)])
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,27 @@
-export class ShoppingListService {
+import { EventEmitter, Injectable } from '@angular/core';
 
+import {Ingredient} from '../shared/ingredient.model';
+
+@Injectable()
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 4),
+    new Ingredient('Tomatoes', 5)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.getIngredients());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.getIngredients());
+  }
 }
